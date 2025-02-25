160945843860ec92744c9baaf8cb80682fcf497d
daniel <danikaszman@gmail.com>
2025-02-25 18:11:39 +0100

    Layout kész, már csak kod kell

--- ui.py
+++ ui.py
@@ -1,41 +1,28 @@
 import PySimpleGUI as sg
 import io
 import time
-from picamera2 import Picamera2
+#from picamera2 import Picamera2
 from PIL import Image
 
-# Function to capture an image from the Raspberry Pi camera
-def capture_image(camera):
-    stream = io.BytesIO()
-    camera.capture(stream, format='jpeg')
-    stream.seek(0)
-    image = Image.open(stream)
-    return image
-
-# Function to convert the image to a format suitable for PySimpleGUI
-def convert_image_to_bytes(image):
-    with io.BytesIO() as output:
-        image.save(output, format='PNG')
-        return output.getvalue()
-
-# Initialize the camera
-camera = Picamera2()
+image_frame = sg.Frame(
+    "Előnézet",
+    [[sg.Image(filename="", size=(200, 200), key="-IMAGE-")]],
+    size=(500, 500),
+    relief=sg.RELIEF_SUNKEN,  # Süllyesztett keret hatás
+)
 
 # Define the layout of the GUI
 layout = [
+    [image_frame],
     [sg.Image(key='-IMAGE-')],
-    [sg.Text('Termek neve'), sg.InputText(key='-INPUT1-')],
-    [sg.Text('Lejarati datum'), sg.InputText(key='-INPUT2-')],
-    [sg.Button('Modify Input 1'), sg.Button('Modify Input 2')]
+    [sg.Text('Termék neve:'), sg.InputText(readonly = True, key='-INPUT1-')],
+    [sg.Text('Lejárati dátum:'), sg.InputText(readonly = True, key='-INPUT2-')],
+    [sg.Button('Fényképezés', size=(15, 2), button_color=("white", "blue")), sg.Push(), sg.Button('Mentés', size=(10, 2), button_color=("white", "green"))],
+    
 ]
 
 # Create the window
 window = sg.Window('Raspberry Pi Camera GUI', layout)
-
-# Capture the initial image
-#image = capture_image(camera)
-#image_bytes = convert_image_to_bytes(image)
-#window['-IMAGE-'].update(data=image_bytes)
 
 # Event loop
 while True:
@@ -44,22 +31,12 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
     
-    if event == 'Modify Input 1':
-        # Modify input 1 (for example, append " modified" to the input)
+    if event == 'Fényképezés':
         values['-INPUT1-'] += " modified"
         window['-INPUT1-'].update(values['-INPUT1-'])
     
-    if event == 'Modify Input 2':
-        # Modify input 2 (for example, append " modified" to the input)
+    if event == 'Mentés':
         values['-INPUT2-'] += " modified"
         window['-INPUT2-'].update(values['-INPUT2-'])
-    
-    # Capture a new image every second
-    #if time.time() % 1 < 0.1:  # Capture every second
-     #   image = capture_image(camera)
-      #  image_bytes = convert_image_to_bytes(image)
-       # window['-IMAGE-'].update(data=image_bytes)
 
-# Cleanup
-camera.close()
 window.close()
